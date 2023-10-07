4948ed093ec509b98105eae3a4d88a429ec0a2e9
rio <riovectored@gmail.com>
2023-10-07 14:49:15 +0700

    belajar TextButton

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,19 @@ void main() {
 class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 101, 53, 110),
-          leading: Icon(Icons.home, size: 25.0),
-          title: Center(child: Text("Kasir App")),
-          actions: <Widget>[Icon(Icons.search, size: 25.0)],
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 101, 53, 110),
+        leading: Icon(Icons.home, size: 25.0),
+        title: Center(child: Text("Kasir Apps")),
+        actions: <Widget>[Icon(Icons.search, size: 25.0)],
+      ),
+      body: TextButton(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
         ),
-        body: Container(
-            color: const Color.fromARGB(255, 185, 92, 142),
-            width: 400.0,
-            height: 200.0,
-            child: Center(
-                child: Center(
-              child: Icon(Icons.android, color: Colors.white, size: 80.0),
-            ))));
+        onPressed: () {},
+        child: Text('TextButton'),
+      ),
+    );
   }
 }
