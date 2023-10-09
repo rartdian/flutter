80947afbce41f9696313dca400ef61c770cf2b7d
rio <riovectored@gmail.com>
2023-10-09 17:38:10 +0700

    TextField

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,15 @@ void main() {
 class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 101, 53, 110),
-        leading: Icon(Icons.home, size: 25.0),
-        title: Center(child: Text("Kasir Apps")),
-        actions: <Widget>[Icon(Icons.search, size: 25.0)],
-      ),
-      body: TextButton(
-        style: ButtonStyle(
-          foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 101, 53, 110),
+          leading: Icon(Icons.home, size: 25.0),
+          title: Center(child: Text("Kasir Apps")),
+          actions: <Widget>[Icon(Icons.search, size: 25.0)],
         ),
-        onPressed: () {},
-        child: Text('TextButton'),
-      ),
-    );
+        body: TextField(
+            decoration: InputDecoration(
+          hintText: 'nama',
+        )));
   }
 }
